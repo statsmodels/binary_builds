8ebb2b9fc1f8479d1db74d70961cfe641f91b649
Skipper Seabold <jsseabold@gmail.com>
2014-03-27 15:51:00 -0400

    Add Python 3.4 to builds

--- build_bdists.bat
+++ build_bdists.bat
@@ -14,23 +14,25 @@ call python %BUILDDIR%
 if errorlevel 1 (
     rem Build the binaries
     call %BUILDDIR%\build_win_bdist64-py26.bat
-    call %BUILDDIR%\build_win_bdist32-py26.bat
-    call %BUILDDIR%\build_win_bdist64-py27.bat
-    call %BUILDDIR%\build_win_bdist32-py27.bat
-    call %BUILDDIR%\build_win_bdist32-py32.bat
-    call %BUILDDIR%\build_win_bdist64-py32.bat
-    call %BUILDDIR%\build_win_bdist32-py33.bat
-    call %BUILDDIR%\build_win_bdist64-py33.bat
-    call python %STATSMODELSDIR%\setup.py sdist --formats=zip,gztar
-    
-    rem Rename the built files to contain the git revision
-    call python %BUILDDIR%\rename_binaries.py
-    
-    rem Remove old files
-    psftp.exe -i C:\Users\skipper\ssh\sourceforge.ppk jseabold,statsmodels@web.sourceforge.net -b %BUILDDIR%\delete_remote_binaries.scr
-    rem Upload new ones
-    pscp.exe -i C:\Users\skipper\ssh\sourceforge.ppk %STATSMODELSDIR%\dist\* jseabold,statsmodels@web.sourceforge.net:htdocs/binaries/
-    
-    rem Email that everything went okay
-    call python %BUILDDIR%\email_update.py 
+       call %BUILDDIR%\build_win_bdist32-py26.bat
+       call %BUILDDIR%\build_win_bdist64-py27.bat
+       call %BUILDDIR%\build_win_bdist32-py27.bat
+       call %BUILDDIR%\build_win_bdist32-py32.bat
+       call %BUILDDIR%\build_win_bdist64-py32.bat
+       call %BUILDDIR%\build_win_bdist32-py33.bat
+       call %BUILDDIR%\build_win_bdist64-py33.bat
+       call %BUILDDIR%\build_win_bdist32-py34.bat
+       call %BUILDDIR%\build_win_bdist64-py34.bat
+       call python %STATSMODELSDIR%\setup.py sdist --formats=zip,gztar
+       
+       rem Rename the built files to contain the git revision
+       call python %BUILDDIR%\rename_binaries.py
+       
+       rem Remove old files
+       psftp.exe -i C:\Users\skipper\ssh\sourceforge.ppk jseabold,statsmodels@web.sourceforge.net -b %BUILDDIR%\delete_remote_binaries.scr
+       rem Upload new ones
+       pscp.exe -i C:\Users\skipper\ssh\sourceforge.ppk %STATSMODELSDIR%\dist\* jseabold,statsmodels@web.sourceforge.net:htdocs/binaries/
+       
+       rem Email that everything went okay
+       call python %BUILDDIR%\email_update.py 
 )
